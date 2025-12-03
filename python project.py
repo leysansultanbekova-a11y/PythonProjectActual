dad1fc1471fb0fd01fc15b752f8f086e96d7b151
leysansultanbekova-a11y <leysan.sultanbekova@gmail.com>
2025-12-02 19:23:46 -0500

    Update python project.py

--- python project.py
+++ python project.py
@@ -5,9 +5,9 @@ pygame.init()
 
 ROWS = 4
 COLS = 4
-CELL_SIZE = 200
+CELL_SIZE = 175
 WIDTH = COLS * CELL_SIZE
-HEIGHT = ROWS * CELL_SIZE
+HEIGHT = (ROWS * CELL_SIZE)+50
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Connecting 4x4")
@@ -15,6 +15,7 @@ pygame.display.set_caption("Connecting 4x4")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (128, 128, 128)
+RED = (255, 0, 0)
 CATEGORY_COLORS = {
     "first": (255, 255, 200),     
     "contests": (144, 244, 144), 
@@ -46,6 +47,9 @@ locked_colors = []
 for row in range(ROWS):
     locked_colors.append([None] * COLS)
 
+attempts = 0
+max_attempts = 4
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -53,6 +57,9 @@ while True:
             sys.exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
+                if attempts >= max_attempts:
+                    continue
+                
                 x, y = event.pos
                 col = x // CELL_SIZE
                 row = y // CELL_SIZE
@@ -69,6 +76,7 @@ while True:
 
                 total_selected = sum(sum(row) for row in selected)
                 if total_selected == 4: 
+                    attempts += 1
                     selected_words = []
                     selected_position = []
                     for row in range(ROWS):
@@ -117,6 +125,14 @@ while True:
                 highlight.fill((128, 128, 128, 120))  
                 screen.blit(highlight, (col * CELL_SIZE, row * CELL_SIZE))
 
+            if attempts >= max_attempts:
+                lost_text = font.render("Sorry, you lost!", True, RED)
+                lost_rect = lost_text.get_rect(center=(WIDTH // 2, HEIGHT -25))
+                screen.blit(lost_text, lost_rect)
+
+
+    attempt_text = font.render(f"Attempts: {max_attempts - attempts}", True, BLACK)
+    screen.blit(attempt_text, (10, HEIGHT - 40))
 
     pygame.display.flip()
 
